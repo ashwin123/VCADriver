0256521b21d4261421bd05ffb6d497878da81270
Ashwin Naresh <ashwin.naresh94@gmail.com>
2015-07-08 16:42:41 +0530

    Initial template

---
+++ vcloudair.py
@@ -0,0 +1,18 @@
+﻿class VCloudDriver:
+	"""
+	An API driver for accessing the various services of VCloud Air On Demand
+	 
+	"""
+    def __init__(self,username=username,password=password):
+		self.username = username;
+		self.password = password;
+		self.basic_authorization = "";
+		self.xvcloud_authorization = "";
+
+	def login(self):
+		pass
+
+
+
+
+
