4b9473c77f1eaeadce73cbacce98db131e1fa003
Ashwin Naresh <ashwin.naresh94@gmail.com>
2015-07-08 19:39:16 +0530

    Login implemented

--- vcloudair.py
+++ vcloudair.py
@@ -10,24 +10,30 @@ class VCloudDriver:
 	An API driver for accessing the various services of VCloud Air On Demand
 	 
 	"""
-    def __init__(self,username):
+    def __init__(self,username,organization):
 		self.username = username
-		self.basic_authorization = ""
+		self.organization = organization
 		self.xvcloud_authorization = ""
 
-	def encode(organization,password):
-		encoded_text = self.username + "@" + organization + ":" + password
+	def _encode(password):
+		encoded_text = self.username + "@" + self.organization + ":" + password
 		return base64.b64encode(encoded_text)
 
-	def login(self,organization,password):
+	def login(self,password):
 		"""
 		Login to VCA on demand service and get the OAuth Token
 
 		"""
 		url = "https://us-virginia-1-4.vchs.vmware.com/api/compute/api/sessions" 
-		headers = rest_helper.buildHeaders(Accept="application/*+xml;version=5.11",Authorization="Basic "+encode(organization,password))
+		headers = rest_helper.buildHeaders(Accept="application/*+xml;version=5.11",Authorization="Basic "+_encode(self.organization,password))
 		response = requests.post(url, headers=headers)
-		return response.headers["x-vcloud-authorization"]
+		self.xvcloud_authorization = response.headers["x-vcloud-authorization"]
+
+
+
+
+
+
 
 		
 
