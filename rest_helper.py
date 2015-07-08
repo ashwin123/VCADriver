1949e9ccfc11c06e20e8e7ec0e35196630bf6a20
Ashwin Naresh <ashwin.naresh94@gmail.com>
2015-07-08 17:35:49 +0530

    Initial template

---
+++ rest_helper.py
@@ -0,0 +1,15 @@
+"""
+A helper module for building the HTTP Requests and evaluating the HTTP Responses
+
+"""
+import xml.etree
+
+def buildHeaders(**kwargs):
+	"""
+	Return a dictionary containing the appropriate headers
+	"""
+	return kwargs
+
+
+
+
