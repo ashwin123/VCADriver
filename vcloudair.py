63088170e2594467c61b85f6682faea3e5477a6f
Ashwin Naresh <ashwin.naresh94@gmail.com>
2015-07-09 13:35:21 +0530

    deployvappdone

--- vcloudair.py
+++ vcloudair.py
@@ -1,6 +1,6 @@
 ﻿import requests
 import urllib2
-import xml.etree
+import xml.etree.ElementTree as ET
 import base64
 
 import rest_helper
@@ -15,7 +15,7 @@ class VCloudDriver:
 		self.username = username
 		self.organization = organization
 		self.xvcloud_authorization = ""
-		self.baseCloudXML="";
+		self.baseCloudXML = "";
 
 	def _encode(password):
 		encoded_text = self.username + "@" + self.organization + ":" + password
@@ -37,8 +37,8 @@ class VCloudDriver:
 		Retrieve XML representation of the organization
 
 		"""
-		url = xml_helper.getHref(baseCloudXML,'orgList')
-		headers = rest_helper.buildHeaders(Accept="application/*+xml;version=5.11",x-vcloud-authorization=xvcloud_authorization)
+		url = xml_helper.getHref(baseCloudXML,"orgList")
+		headers = rest_helper.buildHeaders(Accept="application/*+xml;version=5.11",x-vcloud-authorization=self.xvcloud_authorization)
 		response = requests.get(url, headers=headers)
 		return response.content
 		
@@ -48,7 +48,7 @@ class VCloudDriver:
 
 		"""
 		url = xml_helper.getHref(self.getOrganisation(),'catalog')
-		headers = rest_helper.buildHeaders(Accept="application/*+xml;version=5.11",x-vcloud-authorization=xvcloud_authorization)
+		headers = rest_helper.buildHeaders(Accept="application/*+xml;version=5.11",x-vcloud-authorization=self.xvcloud_authorization)
 		response = requests.get(url, headers=headers)
 		return response.content
 		
@@ -58,7 +58,7 @@ class VCloudDriver:
 
 		"""
 		url = xml_helper.getHref(self.getCatalog(),'catalogItem')
-		headers = rest_helper.buildHeaders(Accept="application/*+xml;version=5.11",x-vcloud-authorization=xvcloud_authorization)
+		headers = rest_helper.buildHeaders(Accept="application/*+xml;version=5.11",x-vcloud-authorization=self.xvcloud_authorization)
 		response = requests.get(url, headers=headers)
 		return response.content
 			
@@ -67,20 +67,28 @@ class VCloudDriver:
 		Retrieve XML representation of the organization
 
 		"""
-		url = xml_helper.getHref(self.getOrganisation,'vdc')
-		headers = rest_helper.buildHeaders(Accept="application/*+xml;version=5.11",x-vcloud-authorization=xvcloud_authorization)
+		url = xml_helper.getHref(self.getOrganisation(),'vdc')
+		headers = rest_helper.buildHeaders(Accept="application/*+xml;version=5.11",x-vcloud-authorization=self.xvcloud_authorization)
 		response = requests.get(url, headers=headers)
 		return response.content
 		
-	def getVappTemplate(self)
+	def getVappTemplate(self):
 		return xml_helper.getHref(self.getVDC(),'instantiateVAppTemplateParams')
-	def deployVapp(self,name):
-		url = xml_helper.getHref(self.getCatalog(),'catalogItem')
-		headers = rest_helper.buildHeaders(Accept="application/*+xml;version=5.11",x-vcloud-authorization=xvcloud_authorization)
-		response = requests.get(url, headers=headers)
-		return response.content
 
-	def deploy_vApp(self,name):
+	def deployVapp(self,name,description):
+		url = "https://us-virginia-1-4.vchs.vmware.com/api/compute/api/vdc/a6d53866-3657-4f11-8378-ec83ffd663d7/action/instantiateVAppTemplate"
+		#url = xml_helper.getHref(self.getVDC(),"instantiateVAppTemplateParams")
+		template = ET.parse("I:/ashwin/vapp.xml")
+		template.set("name",name)
+		template[0].text = description
+		headers = rest_helper.buildHeaders(Accept="application/*+xml;version=5.11",x-vcloud-authorization=self.xvcloud_authorization,Content-Type="application/vnd.vmware.vcloud.instantiateVAppTemplateParams+xml")
+		payload = template
+		response = requests.post(url, headers=headers, data=payload)
+		if response.status_code == requests.codes.ok:
+			#print "VM created successfully"
+			return "Success"
+		else:
+			return "Fail"
 
 
 
--- xml_helper.py
+++ xml_helper.py
@@ -1,12 +1,13 @@
 """
 Module which helps find elements inside xml response
+
 """
 from xml.etree import ElementTree as ET
 
 def getHref(xmlText,key):
 	root = EL.fromstring(xmlText)
 	for EL in root:
-		t=EL.get('type')
+		t = EL.get('type')
 		#print(t)
 		#print(str(t.find(key+'+xml')))
 		if t.find(key+'+xml') != -1:
